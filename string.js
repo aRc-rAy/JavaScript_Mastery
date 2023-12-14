79448cd390542aab7e22732c4d034a6dc13e39bb
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2023-12-14 22:52:15 +0530

    String version

---
+++ string.js
@@ -0,0 +1,22 @@
+// console.log("Hello" + 45);
+let name = "Dilip";
+// console.log(`Hi there this is modern style of writing, got it ${name}`);
+
+let str = "Dilip";
+// console.log(typeof str);
+// console.log(str.charAt(0));
+
+let newString = str.substring(0, 3);
+// console.log(newString);
+
+str = "This is the large string for test purpose";
+
+console.log(str.length);
+console.log(str.includes("is"));
+console.log(str.startsWith("This"));
+console.log(str.endsWith("ose"));
+console.log(str.indexOf("is", 3));
+console.log(str.slice(5, 10));
+console.log(str.toUpperCase());
+console.log(str.replace("This is the large string for", "Best"));
+// console.log(str.replaceAll("This is the large string for", "Best"));
