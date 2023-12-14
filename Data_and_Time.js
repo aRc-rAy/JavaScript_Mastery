db3eba51fcd72e11489bf0d4b98097d46b000bfa
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2023-12-15 00:16:07 +0530

    simple date and time reading

---
+++ Data_and_Time.js
@@ -0,0 +1,8 @@
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(Date.now());
+console.log(myDate.toTimeString());
+console.log(myDate.toDateString());
+
