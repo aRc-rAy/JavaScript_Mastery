4d44354dc327444cbfef4510e9e3f5c8e1ba060c
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2023-12-15 01:01:26 +0530

    Array methods

---
+++ Basics_1/array.js
@@ -0,0 +1,23 @@
+let arr = [100, 100, "Araaya hai", "ye bhi hai", 99, 1, 11, 10000, 100000];
+// console.log(arr);
+
+// arr.unshift(99);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.indexOf(100));
+// console.log(arr.includes(1));
+
+// console.log(arr.join());
+// return string with comma separated values
+
+//slice splice
+console.log("A ", arr);
+
+console.log(arr.slice(1, 3));
+console.log("B ", arr);
+
+console.log(arr.splice(1, 3));
+console.log("C ", arr);
