f6f1d9206214d39a2f31b4b1cb47812f7ce15de8
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2023-12-15 13:27:04 +0530

    iffe statements

---
+++ Basics_2/04_iffe.js
@@ -0,0 +1,17 @@
+// (function chai() {  //name iffe++++++++++++++++++++++++++++
+// 	console.log("This is iffe function");
+// })();
+//
+//
+//
+// (() => {
+// 	console.log(`this is connected`);
+// })();
+//
+//
+//
+//
+//
+// ((name) => {
+// 	console.log(`this is connected okk ${name}`);
+// })("Dilip");
